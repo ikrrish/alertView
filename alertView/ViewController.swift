cf6f3490c3e9af8ad7dfbf1b98a7a2191b1cd3fe
Kishan Govindiya <kishangovindiya1@gmail.com>
2023-03-27 11:14:25 +0530

    alertView

--- alertView/ViewController.swift
+++ alertView/ViewController.swift
@@ -11,9 +11,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+    }
+    func showAlert(){
+        let alert = UIAlertController(title: "hello", message: "i'm Kishan", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "BACK", style: .destructive, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 
-
+    @IBAction func alertButton(_ sender: Any) {
+        showAlert()
+    }
 }
 
